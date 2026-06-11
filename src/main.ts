4bbdf5b9b84c55885a9efd5d30794d9de4d9ee05
actionchain <actionchain@chainguard.dev>
2026-06-11 14:35:57 +0000

    Update int128/kaniko-action@v1.61.0

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+import { run } from './run.js'
+
+const main = async (): Promise<void> => {
+  const outputs = await run({
+    executor: core.getInput('executor', { required: true }),
+    cache: core.getBooleanInput('cache'),
+    cacheRepository: core.getInput('cache-repository'),
+    cacheTTL: core.getInput('cache-ttl'),
+    pushRetry: core.getInput('push-retry'),
+    registryMirrors: core.getMultilineInput('registry-mirror'),
+    verbosity: core.getInput('verbosity'),
+    kanikoArgs: core.getMultilineInput('kaniko-args'),
+    dockerRunArgs: core.getMultilineInput('docker-run-args'),
+    buildArgs: core.getMultilineInput('build-args'),
+    context: core.getInput('context'),
+    file: core.getInput('file'),
+    labels: core.getMultilineInput('labels'),
+    push: core.getBooleanInput('push'),
+    tags: core.getMultilineInput('tags'),
+    target: core.getInput('target'),
+  })
+  core.setOutput('digest', outputs.digest)
+}
+
+main().catch((e: Error) => {
+  core.setFailed(e)
+  console.error(e)
+})
